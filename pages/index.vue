5303c270a29d97ec3f0158f9247e9432c371d112
Maksiks <maksiksking@gmail.com>
2025-03-09 19:18:49 +0200

    vercel fix attempt x21

--- pages/index.vue
+++ pages/index.vue
@@ -6,41 +6,7 @@ import { useRuntimeConfig } from '#imports';
 
 import {saveShelf, deleteShelf, getShelf, getAllShelves, clearShelves} from "~/utils/indexedDB";
 
-const shelves: any = ref([])
 
-const elem1Content = ref<string>('')
-const shelfElem = ref<HTMLElement | null>(null)
-
-watch(shelfElem, (newVal) => {
-  if (newVal) {
-    const elChild = newVal.firstElementChild as HTMLElement;
-    if (elChild) {
-      trackedElems.push(elChild); // Now tracking the actual DOM element
-      updatePositions(); // Apply transformations after it's mounted
-    }
-  }
-});
-
-async function loadShelves(): Promise<any> {
-  console.log('Loading shelves...')
-
-  // Take the shelf from the DB, turn it into a real, tangible element
-  const _string = await getShelf(1);
-  const _div: HTMLElement = document.createElement('div');
-  // here we write down the element as a string (indexedDB can't store HTML
-  // elems but we need it anyways so it's a good thing), then we send it to
-  // the template's v-html which cooks magic, and we also write down the div
-  // as a ref
-  elem1Content.value = _string.html;
-  _div.innerHTML = _string.html;
-  // extract the child to prevent an unintended wrapper
-  // shelves.value[0] = _div;
-  // trackedElems.push(_div)
-}
-
-async function handleTest(): Promise<void> {
-  await saveShelf(trackedElems[0], 1);
-}
 
 const scale = ref(1)
 const translateX = ref(0)
@@ -127,11 +93,60 @@ const trackedElems: Array<any> = reactive([])
 const screenX = ref(450)
 const screenY = ref(450)
 
+function updatePositions() {
+  const offsetX = ref(0)
+  const offsetY = ref(0)
+  trackedElems.forEach((el: HTMLElement, i) => {
+    if (!el) return;
+
+    // context: v-html always puts the thing in a wrapper
+    // to avoid that we use the child
+    const elChild = el.childNodes[0] as HTMLElement;
+    if (!elChild) return;
+    console.log(elChild)
+
+    const worldX = worldElementPos.value.x + offsetX.value
+    const worldY = worldElementPos.value.y + offsetY.value
+
+    screenX.value = worldX * scale.value + translateX.value
+    screenY.value = worldY * scale.value + translateY.value
+
+    el.style.transform = `translate(${screenX.value}px, ${screenY.value}px) scale(${scale.value})`
+
+    offsetX.value += 75;
+  })
+}
+
+
+
+const items = ref([
+  {name: "Test", id: 0},
+  {name: "Teest", id: 1},
+  {name: "Teesst", id: 2}
+],)
+
+// in the future i can do it for v-fors
+const setElems = (el: HTMLElement) => {
+  if (el) trackedElems.push(el);
+};
+
+const shelfComp = ref<HTMLCanvasElement | null>(null);
+
 onMounted(() => {
   if (import.meta.client) {
     console.log('Running on client');
   }
+  console.log("hey at least this works right")
+  loadShelves();
 
+  // defs for elems manually for now
+  // @ts-ignore
+  const shelfWrapper: HTMLElement = shelfComp.value?.shelfWrapper;
+  if (shelfWrapper) trackedElems.push(shelfWrapper);
+
+
+  resizeCanvas()
+  window.addEventListener('resize', resizeCanvas)
 })
 </script>
 
